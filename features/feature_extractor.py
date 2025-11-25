0812273e0d56b76571348ee8f28936ffb81f703f
Mohamed-Ashraf273 <ma2736666@gmail.com>
2025-11-25 14:39:03 +0200

    remove features dir

--- features/feature_extractor.py
+++
@@ -1,6 +0,0 @@
-class FeatureExtractor:
-    def __init__(self, text):
-        self.text = text
-
-    def extract_features(self):
-        raise NotImplementedError("Subclasses should implement this method")
